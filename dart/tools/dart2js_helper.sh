b05cebf2fe0f40a1ce597861901b99362ae97804
Chris Bracken <chris@bracken.jp>
2016-08-27 16:32:43 -0700

    Suppress leading whitespace from wc output
    This prevents whitespace-related diffs in the deferred part count
    comparison on line 13.

--- dart/tools/dart2js_helper.sh
+++ dart/tools/dart2js_helper.sh
@@ -9,7 +9,7 @@ shift 3
 
 "$@"
 
-actual_count=$(find "$out_dir" -name "$out_js_*.part.js" | wc -l)
+actual_count=$(find "$out_dir" -name "$out_js_*.part.js" -maxdepth 1 | wc -l | sed -e 's/^[ \t]*//')
 if [[ $actual_count != $expected_count ]]; then
   echo "ERROR: Expected $expected_count deferred library outputs, but found $actual_count."
   echo "Set deferred_lib_count=$actual_count on $target."
